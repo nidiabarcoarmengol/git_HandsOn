88fc9d07e60035746f091424f6221bba71a1b3d8
Nidia Barco Armengol <nidiabarco16@gmail.com>
2024-01-31 17:26:12 +0100

    Correct commit

--- seqClass.py
+++ seqClass.py
@@ -13,6 +13,7 @@ if len(sys.argv) == 1:
 args = parser.parse_args()
 
 args.seq = args.seq.upper()                 # Note we just added this line
+parser.add_argument("-m", "--motif", type = str, required = False, help = "Motif")
 if re.search('^[ACGTU]+$', args.seq):
     if re.search('T', args.seq):
         print ('The sequence is DNA')
@@ -31,4 +32,3 @@ if args.motif:
     else:
         print("NOT FOUND")
 
-parser.add_argument("-m", "--motif", type = str, required = False, help = "Motif")
